a4409612f06962a8ba5265930e86d2733bd19f98
SARAN PRASATH <saranprasath.official@gmail.com>
2026-09-01 17:50:20 +0530

    feat: enable client-side routing

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import './styles/index.css';
+import './styles/auth.css';
 import { App } from './App';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </StrictMode>,
 );
